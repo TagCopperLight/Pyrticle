541fbdd7a98222d3e16cc3d3d5571e454bf73c4a
TagCopperLight <tagcopperlight@gmail.com>
2022-12-24 23:57:25 +0100

    Pyrticule -> Pyrticle

--- pyrticle/maths.py
+++ pyrticle/maths.py
@@ -1,12 +1,12 @@
-from . import particule
+from . import particle
 
 
-def fade_color(particle: particule.Particle, color: tuple[int, int, int], progress: float) -> tuple[int, int, int]:
+def fade_color(particle: particle.Particle, color: tuple[int, int, int], progress: float) -> tuple[int, int, int]:
     calculated_color = (int(particle.shape.orig_color()[i] + (color[i] - particle.shape.orig_color()[i]) * progress) for i in range(3))
     return tuple(calculated_color)
 
-def fade_alpha(particle: particule.Particle, alpha: int, progress: float) -> int:
+def fade_alpha(particle: particle.Particle, alpha: int, progress: float) -> int:
     return int(particle.shape.orig_alpha() + (alpha - particle.shape.orig_alpha()) * progress)
 
-def fade_alpha_exp(particle: particule.Particle, alpha: int, progress: float) -> int:
+def fade_alpha_exp(particle: particle.Particle, alpha: int, progress: float) -> int:
     return int(particle.shape.orig_alpha() + (alpha - particle.shape.orig_alpha()) * progress ** 3)
